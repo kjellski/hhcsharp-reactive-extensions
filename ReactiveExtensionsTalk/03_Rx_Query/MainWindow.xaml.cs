e0ead6d4675a29b1f8dcf24e890aa89945eed04d
Kjell Otto <otto.kjell@gmail.com>
2014-09-14 14:50:59 +0100

    added throttle demo with words file!

--- ReactiveExtensionsTalk/03_Rx_Query/MainWindow.xaml.cs
+++ ReactiveExtensionsTalk/03_Rx_Query/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Windows;
-using System.Windows.Documents;
+using System.Windows.Controls;
 
 namespace _03_Rx_Query
 {
@@ -11,18 +14,44 @@ namespace _03_Rx_Query
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> wordList;
+        private List<string> _wordList;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeWords();
+            
+            var textBoxText = SetupSearchQuery();
+
+            textBoxText
+                //.Throttle(TimeSpan.FromMilliseconds(500))
+                //.ObserveOn(DispatcherScheduler.Current)
+                .Subscribe(s => 
+                    FillListBoxWith(_wordList.Where(w => w.Contains(s)).ToList()));
+
+            FillListBoxWith(_wordList);
+        }
+
+        private IObservable<string> SetupSearchQuery()
+        {
+            return Observable.FromEventPattern(TextBox, "TextChanged")
+                .Select(e => ((TextBox)e.Sender).Text);
         }
 
         private void InitializeWords()
         {
-            wordList = File.ReadAllLines(@"..\..\words.txt").ToList();
-            wordList.ForEach(item => ListBox.Items.Add(item));
+            _wordList = File.ReadAllLines(@"..\..\words.txt").ToList();
+        }
+
+        private void FillListBoxWith(List<string> words)
+        {
+            ListBox.Items.Clear();
+            words.ForEach(item => ListBox.Items.Add(item));            
+        }
+
+        private void TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // why does this suck so much???
         }
     }
 }
