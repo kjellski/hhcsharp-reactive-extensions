63a51f4eb8a0f91376a7f14935bcf763f552f552
Kjell Otto <otto.kjell@gmail.com>
2014-09-14 13:48:47 +0100

    added words file and added slurping it

--- ReactiveExtensionsTalk/03_Rx_Query/MainWindow.xaml.cs
+++ ReactiveExtensionsTalk/03_Rx_Query/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _03_Rx_Query
 {
@@ -20,9 +11,18 @@ namespace _03_Rx_Query
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> wordList;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeWords();
+        }
+
+        private void InitializeWords()
+        {
+            wordList = File.ReadAllLines(@"..\..\words.txt").ToList();
+            wordList.ForEach(item => ListBox.Items.Add(item));
         }
     }
 }
